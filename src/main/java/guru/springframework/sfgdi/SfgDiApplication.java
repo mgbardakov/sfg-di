49064c49dce2245d9fdb17074311ab37cc125afe
mgbardakov <mgbardakov@gmail.com>
2021-03-21 18:25:53 +0300

    properties

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
 import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -46,6 +47,11 @@ public class SfgDiApplication {
 		System.out.println(prototypeBean1.getScope());
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getScope());
+
+		FakeDataSource fds = ctx.getBean(FakeDataSource.class);
+		System.out.println(fds.getUsername());
+		System.out.println(fds.getPassword());
+		System.out.println(fds.getJdbcURL());
 
 	}
 
--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,17 +2,31 @@ package guru.springframework.sfgdi.config;
 
 import com.springframework.pets.PetService;
 import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 /**
  * Created by jt on 2/20/21.
  */
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}")String password,
+                                  @Value("${guru.jdbcURL}")String url) {
+        var fds = new FakeDataSource();
+        fds.setUsername(username);
+        fds.setPassword(password);
+        fds.setJdbcURL(url);
+        return fds;
+    }
 
     @Bean
     PetServiceFactory petServiceFactory(){
